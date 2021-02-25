f12df5f8422c7390999f059678016893c2de9d84
Mara Rúbia Alves de Oliveira <40505976+mararbia@users.noreply.github.com>
2021-02-25 09:09:03 -0300

    feat: Experience bar implementation

--- moveit/src/App.tsx
+++ moveit/src/App.tsx
@@ -1,19 +1,10 @@
-import { Button } from './components/Button'
+import { ExperienceBar } from './components/ExperienceBar';
 import './styles/global.css';
 
 function App() {
   return (
-    <div>
-      <Button color="red">
-        Botão 1 
-        {/* conteúdo que estará dentro do children  */}
-      </Button>
-      <Button color="blue">
-        Botão 2
-      </Button>
-      <Button color="green">
-        Botão 3
-      </Button>
+    <div className="container">
+      <ExperienceBar />
     </div>
   );
 }
