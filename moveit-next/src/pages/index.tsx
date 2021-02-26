6b15051adb0ac664722ab411ab2c53806100a405
Mara Rúbia Alves de Oliveira <40505976+mararbia@users.noreply.github.com>
2021-02-25 23:43:44 -0300

    feat: Added title and icon

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -3,10 +3,16 @@ import { Countdown } from "../components/CountDown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 import styles from "../styles/pages/Home.module.css";
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+
       <ExperienceBar />
 
     <section>
