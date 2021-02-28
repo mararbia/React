049027199505ee896799624c35d6de859a11bae6
Mara Rúbia Alves de Oliveira <40505976+mararbia@users.noreply.github.com>
2021-02-28 15:18:29 -0300

    feat: Leveling structure added

--- moveit-next/src/pages/_app.tsx
+++ moveit-next/src/pages/_app.tsx
@@ -2,12 +2,15 @@ import { ChallengesProvider } from '../contexts/ChallengesContext'
 import '../styles/global.css' 
 
 import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownProvider } from '../contexts/CountdownContext';
 
 function MyApp({ Component, pageProps }) {
 
   return (
     <ChallengesProvider>
-      <Component {...pageProps} />
+      <CountdownProvider>
+        <Component {...pageProps} />
+      </CountdownProvider>
     </ChallengesProvider>
   )
   
