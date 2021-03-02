38cd0e4495cbd411ddf73292b0c8f19220e6fa54
Mara Rúbia Alves de Oliveira <40505976+mararbia@users.noreply.github.com>
2021-03-02 15:12:58 -0300

    feat: Implementation of cookies

--- moveit-next/src/pages/_app.tsx
+++ moveit-next/src/pages/_app.tsx
@@ -7,13 +7,8 @@ import { CountdownProvider } from '../contexts/CountdownContext';
 function MyApp({ Component, pageProps }) {
 
   return (
-    <ChallengesProvider>
-      <CountdownProvider>
-        <Component {...pageProps} />
-      </CountdownProvider>
-    </ChallengesProvider>
+    <Component {...pageProps} />
   )
-  
 }
 
 export default MyApp
