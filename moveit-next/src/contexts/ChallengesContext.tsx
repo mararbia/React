91344789dbbc2919b0b2b2eeecee8d132640a33c
Mara Rúbia Alves de Oliveira <40505976+mararbia@users.noreply.github.com>
2021-03-02 16:30:13 -0300

    fix: CompletedChallenge bug

--- moveit-next/src/contexts/ChallengesContext.tsx
+++ moveit-next/src/contexts/ChallengesContext.tsx
@@ -38,7 +38,7 @@ export function ChallengesProvider({
 
     const [level, setLevelUp] = useState(rest.level ?? 1);
     const [currentExperience, setCurrentExperience] = useState(rest.currentExperience ?? 0);
-    const [challengesCompleted, setChallengesCompleted] = useState(rest.currentExperience ?? 0);
+    const [challengesCompleted, setChallengesCompleted] = useState(rest.challengesCompleted ?? 0);
 
     const [activeChallenge, setActiveChallenge] = useState(null);
     const [isLevelUpModalOpen, setIsLevelUpModalOpen] = useState(false);
--- moveit-next/src/pages/_app.tsx
+++ moveit-next/src/pages/_app.tsx
@@ -1,8 +1,4 @@
-import { ChallengesProvider } from '../contexts/ChallengesContext'
 import '../styles/global.css' 
-
-import { ChallengesContext } from '../contexts/ChallengesContext';
-import { CountdownProvider } from '../contexts/CountdownContext';
 
 function MyApp({ Component, pageProps }) {
 
